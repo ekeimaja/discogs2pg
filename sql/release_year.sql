2e063f6f31a4ed31652e57a2f5ac31927251ea2c
ekeimaja <44267894+ekeimaja@users.noreply.github.com>
2019-03-21 17:12:23 +0200

    Update release_year.sql

--- sql/release_year.sql
+++ sql/release_year.sql
@@ -1,7 +1,7 @@
 ALTER TABLE release ADD COLUMN release_year INT NULL;
 
 UPDATE release SET release_year = (
-  CASE WHEN released ~ '^[0-9]{4}'  THEN SUBSTRING(released, 0, 4)::int
+  CASE WHEN released ~ '^[0-9]{4}'  THEN SUBSTRING(released, 0, 5)::int
   ELSE NULL
   END
 );
